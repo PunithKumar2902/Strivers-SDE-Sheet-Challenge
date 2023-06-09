d6547cecd69c3c7124bf4c4ed25aecb27e5ae3e3
Punith Kumar <66864406+PunithKumar2902@users.noreply.github.com>
2023-06-09 23:11:55 +0530

    Create 43.Maximum_meetings.cpp

---
+++ 8.Greedy/43.Maximum_meetings.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+
+static bool comp (pair<int,pair<int,int>> p,pair<int,pair<int,int>> q)
+{
+    if(p.first==q.first) p.second.second<q.second.second;
+
+    return p.first<q.first;
+}
+
+vector<int> maximumMeetings(vector<int> &start, vector<int> &end) {
+    // Write your code here.
+    
+    vector<pair<int,pair<int,int>>> mp;
+
+    int n= start.size();
+    for(int i=0;i<n;i++)
+    {
+        mp.push_back({end[i],{start[i],i+1}});
+    }
+    
+    sort(mp.begin(),mp.end(),comp);
+
+    int last = -1;
+
+    vector<int> ans;
+
+    for(int i=0;i<n;i++)
+    {
+        if(mp[i].second.first>last)
+        {
+            ans.push_back(mp[i].second.second);
+            last = mp[i].first;
+        }
+    }
+
+    return ans;
+}
